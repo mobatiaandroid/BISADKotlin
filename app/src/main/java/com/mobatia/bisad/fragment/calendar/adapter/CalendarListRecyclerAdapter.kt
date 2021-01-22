1c72918f446365f64964108f794223036aefee35
Aparna <aparna.mohan@mobatia.com>
2021-01-22 12:01:25 +0530

    Calendar:Background color set

--- app/src/main/java/com/mobatia/bisad/fragment/calendar/adapter/CalendarListRecyclerAdapter.kt
+++ app/src/main/java/com/mobatia/bisad/fragment/calendar/adapter/CalendarListRecyclerAdapter.kt
@@ -1,10 +1,13 @@
 package com.mobatia.bisad.fragment.calendar.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.annotation.NonNull
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.mobatia.bisad.R
 import com.mobatia.bisad.fragment.calendar.model.CalendarListResponse
@@ -16,11 +19,13 @@ import com.mobatia.bisad.manager.OnBottomReachedListener
     RecyclerView.Adapter<CalendarListRecyclerAdapter.MyViewHolder>() {
      inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var title: TextView = view.findViewById(R.id.title)
+        var realMain: RelativeLayout = view.findViewById(R.id.realMain)
+        var card_view: CardView = view.findViewById(R.id.card_view)
     }
     @NonNull
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.adapter_calender_list, parent, false)
+            .inflate(R.layout.adapter_calendar_new, parent, false)
         return MyViewHolder(itemView)
     }
 
@@ -29,6 +34,8 @@ import com.mobatia.bisad.manager.OnBottomReachedListener
         val summary = calendarArrayList[position]
 
         holder.title.text = summary.title
+        holder.realMain.setBackgroundColor(Color.parseColor(summary.color))
+     //   holder.card_view.setBackgroundColor(Color.parseColor(summary.color))
 
     }
     override fun getItemCount(): Int {
