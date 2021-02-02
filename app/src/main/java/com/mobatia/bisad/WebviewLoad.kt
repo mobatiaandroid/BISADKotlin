ed06651a7c0955794f9d3c6af4aed63f704f3b93
mobatiaandroid <77715032+mobatiaandroid@users.noreply.github.com>
2021-02-02 12:17:01 +0530

    WEBVIEW FIXED-NITHIN

--- app/src/main/java/com/mobatia/bisad/WebviewLoad.kt
+++ app/src/main/java/com/mobatia/bisad/WebviewLoad.kt
@@ -7,15 +7,18 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mobatia.bisad.fragment.home.mContext
+import java.net.URLEncoder
 
 class WebviewLoad : AppCompatActivity() {
 
@@ -55,9 +58,34 @@ class WebviewLoad : AppCompatActivity() {
         webview.settings.allowFileAccess = true
         webview.setBackgroundColor(Color.TRANSPARENT)
         webview.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null)
-        webview.loadUrl("https://docs.google.com/gview?embedded=true&url=" + passedintent)
+
+        webview.webViewClient = WebViewClient()
+
+        //webview.loadUrl("http://www.amazon.com")
+
+
+
+        //webview.loadUrl("https://docs.google.com/gview?embedded=true&url=" + passedintent)
+
+
+        webview.loadUrl("https://docs.google.com/gview?embedded=true&url=" + URLEncoder.encode(passedintent))
+
+       // webview.loadUrl(passedintent)
         Log.e("PDF: ", passedintent)
 
+    }
+    inner class WebViewClient : android.webkit.WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            Log.e("PDFview: ",url)
+
+            view.loadUrl(url)
+            return false
+        }
+
+        override fun onPageFinished(view: WebView, url: String) {
+            super.onPageFinished(view, url)
+            progressdialog.visibility = View.GONE
+        }
     }
 
     override fun onBackPressed() {
